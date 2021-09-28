578da95f091852c3a898165e905215d8584b46aa
novakvova <novakvova@gmail.com>
2021-09-28 17:05:01 +0300

    Add scroll veiw

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText txtData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtData=findViewById(R.id.txtData);
+
+    }
+
+    public void ClickBtnHello(View view) {
+        Toast.makeText(this,txtData.getText(),Toast.LENGTH_LONG).show();
     }
 }
