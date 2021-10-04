b1e9dcd7caf0236d79d70cbae8a1ad836faeccbc
novakvova <novakvova@gmail.com>
2021-10-04 17:51:25 +0300

    Send request webApi ASP.NEt Core 10.0.2.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ClickBtnHello(View view) {
-        Toast.makeText(this,txtData.getText(),Toast.LENGTH_LONG).show();
+        //Toast.makeText(this,txtData.getText(),Toast.LENGTH_LONG).show();
+        MainActivity intasnce = this;
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getWeather()
+                .enqueue(new Callback<List<Post>>() {
+                    @Override
+                    public void onResponse(@NonNull Call<List<Post>> call, @NonNull Response<List<Post>> response) {
+                        List<Post> post = response.body();
+                        //Toast.makeText(intasnce,post.get(0).getTemperatureC(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<List<Post>> call, @NonNull Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
+
     }
 }
