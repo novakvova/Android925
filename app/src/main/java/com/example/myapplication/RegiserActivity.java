d1924b3c3486b5f4d87f81702cd78f40bda1a6a7
novakvova <novakvova@gmail.com>
2021-10-11 15:36:26 +0300

    Add spinner java

--- app/src/main/java/com/example/myapplication/RegiserActivity.java
+++ app/src/main/java/com/example/myapplication/RegiserActivity.java
@@ -4,16 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class RegiserActivity extends AppCompatActivity {
+public class RegiserActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner spinner;
+    private static final String[] paths = {"item 1", "item 2", "item 3"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regiser);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
     }
 
     public void onClickRegister(View view) {
@@ -27,6 +44,31 @@ public class RegiserActivity extends AppCompatActivity {
         }
         else
             emailLayout.setError("");
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                int a=23;
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                int c=23;
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                int b=23;
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 
 }
