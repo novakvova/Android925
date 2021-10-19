d65c65404388b3e789cb5c2d8fefaad3d9e54eae
novakvova <novakvova@gmail.com>
2021-10-19 17:29:35 +0300

    Get token server

--- app/src/main/java/com/example/myapplication/NetworkService.java
+++ app/src/main/java/com/example/myapplication/NetworkService.java
@@ -1,11 +1,12 @@
 package com.example.myapplication;
 
+import com.example.myapplication.constants.Urls;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 public class NetworkService {
     private static NetworkService mInstance;
-    private static final String BASE_URL = "http://10.0.2.2:5000";
+    private static final String BASE_URL = Urls.BASE;
     private Retrofit mRetrofit;
 
     private NetworkService() {
